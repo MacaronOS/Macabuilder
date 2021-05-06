e0fe20beb3199fbfa2ad7b145374023849bb1098
Plunkerusr <slava.pach@gmail.com>
2021-05-07 00:19:22 +0300

    Utils: add Thread safe queue

---
+++ utils/ThreadQueue.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "Lock.h"
+
+#include <queue>
+
+template <typename ValueType>
+class ThreadQueue {
+public:
+    ThreadQueue() = default;
+
+public:
+    size_t size_approx() { return m_queue.size(); }
+
+public:
+    void enqueue(const ValueType& value);
+    bool dequeue(ValueType& value);
+    void enqueue(ValueType&& value);
+
+private:
+    SpinLock m_lock {};
+    std::queue<ValueType> m_queue {};
+};
+
+template <typename ValueType>
+void ThreadQueue<ValueType>::enqueue(const ValueType& value)
+{
+    enqueue(ValueType(value));
+}
+
+template <typename ValueType>
+void ThreadQueue<ValueType>::enqueue(ValueType&& value)
+{
+    auto _ = ScopedLocker(m_lock);
+    m_queue.push(std::move(value));
+}
+
+template <typename ValueType>
+bool ThreadQueue<ValueType>::dequeue(ValueType& value)
+{
+    auto _ = ScopedLocker(m_lock);
+    if (m_queue.empty()) {
+        return false;
+    }
+    value = m_queue.front();
+    m_queue.pop();
+    return true;
+}
